3bd5aa9032173ca5807a10767c16f6c8ab4b98c5
Josua <jg.spielt@gmail.com>
2019-07-11 18:19:29 +0200

    3

--- clickgame/main.js
+++ clickgame/main.js
@@ -1,4 +1,39 @@
-function btnclick()
+window.onload = load();
+
+var clicks = 0;
+
+function load()
 {
-    alert("click");
+    clicks = parseInt(getCookie("clicks"));
+}
+
+function click()
+{
+    clicks++;
+    setCookie("clicks", clicks, 300);
+}
+
+function setCookie(name, value, exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name)
+{
+    var name = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
 }
