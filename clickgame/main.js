599ceb42820369b9adff07ed79ad192c4dafe8d7
Josua <jg.spielt@gmail.com>
2019-07-11 19:08:59 +0200

    5

--- clickgame/main.js
+++ clickgame/main.js
@@ -1,21 +1,35 @@
 var clicks = 0;
+var upgrades = 0;
 
 function load()
 {
     clicks = parseInt(getCookie("clicks"));
+    upgrades = parseInt(getCookie("upgrades"));
     update();
 }
 
 function update()
 {
     document.getElementById("p1").innerHTML = "Klicks: " + clicks;
+    document.getElementById("upgradebtn").innerHTML = "Upgrades ("+upgrades+"): " + upgradeCost;
     setCookie("clicks", clicks, 300);
+    setCookie("upgrades", upgrades, 300);
 }
 
 function btnclick()
 {
-    clicks++;
+    clicks += Math.pow(2, upgrades);
     update();
+}
+
+const upgradeCost = Math.pow(3, upgrades);
+function upgrade()
+{
+    if (clicks >= upgradeCost)
+    {
+        clicks -= upgradeCost;
+        upgrades++;
+    }
 }
 
 function setCookie(name, value, exdays)
